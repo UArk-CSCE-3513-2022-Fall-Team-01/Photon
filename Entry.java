a4c05bbfb5898d176a682495c1df6795b54ca371
leogj99 <leoguzmanj99@gmail.com>
2022-10-08 22:03:07 -0500

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -1,12 +1,13 @@
-// Entry Screen class
+import java.awt.Dimension;
 import java.awt.Color;
-
 import javax.swing.*;
+import javax.swing.plaf.DimensionUIResource;
 
-class Entry {
+class Entry extends JFrame{
     JFrame frame;
     JPanel panel1;
     JPanel panel2;
+    JTable table1;
 
     int x1 = 200;                          // X-Cordinate of panel1.
     int y1 = 100;                          // Y-Cordinate of panel.
@@ -18,15 +19,30 @@ class Entry {
     int w2 = w1;                           // Width of panel2.
     int h2 = h1;                           // Height of panel2.
 
+    int rows = 2;                          // Jtable rows.
+    int columns = 5;                       // Jtable columns.
+
+    String[] tableHeader = {"ID", "CODENAME"};
+    String[][] tableColumns = {
+        {" ", " "},
+        {" ", " "},
+        {" ", " "},
+        {" ", " "},
+        {" ", " "}
+    };
+
     public Entry(){
         frame = new JFrame("Laser Tag");
         panel1 = new JPanel();
         panel2 = new JPanel();
+        table1 = new JTable(tableColumns, tableHeader);
 
-        frame();
-        teamPanel();
+
+        addFrame();
+        addPanel();
+        addTable();
     }
-    public void frame(){
+    public void addFrame(){
         frame.setVisible(true);                                    // Makes frame visible.
         frame.setLayout(null);                               // Null because we are adding components manually.
         frame.setSize(1000, 1000);                     // Size frame.
@@ -36,7 +52,7 @@ class Entry {
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);         // Finishes the program when we press the X bottom.
 
     }
-    public void teamPanel(){
+    public void addPanel(){                                          // Method that created the team panels
         panel1.setBounds(x1, y1, w1, h1);
         panel1.setBackground(Color.RED);
 
@@ -45,6 +61,9 @@ class Entry {
         
         frame.add(panel1);                                            // Panels added to the frame.
         frame.add(panel2);
-
+    }
+    public void addTable(){                                           // Creates the input fields for the team panels 
+        JScrollPane scrollPane = new JScrollPane(table1);             // Without this the table has no labels 
+        panel1.add(scrollPane);                                       // Adds table to the panel
     }
 }
