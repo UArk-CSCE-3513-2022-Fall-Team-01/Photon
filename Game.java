790e6aa327dd3020d91ad206d3974c03087edb2b
bakinser <bakinser@gmail.com>
2022-10-09 12:08:50 -0500

    Splash screen now starts before Entry screen

--- Game.java
+++ Game.java
@@ -1,6 +1,8 @@
 public class Game{
     public static void main(String[] args){
-        //Splash splash = new Splash();
+        Splash splash = new Splash(3000); //Create a splash screen that lasts for 3 seconds
+		splash.show(); //Show the splash screen
+		
         Entry entry = new Entry();
     }
 }
--- Splash.java
+++ Splash.java
@@ -2,34 +2,36 @@ import java.awt.Image;
 import javax.swing.*;
 
 class Splash {
-    JFrame frame;                 // GUI window to add components.
-    JLabel label;                 // GUI display area for strings or images. 
 
-    public Splash(){
-        label = new JLabel();
-        frame = new JFrame();
-
-        addImage();               // Adds image to the frame.
-        createFrame();            // Creates the frame.
+	private int t; //The time before the splash screen hides
+	
+    public Splash(int time){
+		t = time;
     }
-    public void createFrame(){
-        frame.setUndecorated(true);                          // Gets rid of the tittle bar
+    public void show(){
+		JFrame frame = new JFrame();
+        frame.setUndecorated(true);                          // Gets rid of the title bar
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setSize(871, 555);                 // Sets the size of the window.
         frame.setLocationRelativeTo(null);                   // Sets the location to the center of the screen. 
-        frame.setVisible(true);                              // Makes the window visible.
         frame.setResizable(false);                   // Prevents the manual resize of the window. 
-    }
-    public void addImage(){        
-        ImageIcon image = new ImageIcon("logo.jpg");    // Creates ImageIcon component. 
+		
+		ImageIcon image = new ImageIcon("images/photon.jpg");    // Creates ImageIcon component. 
         Image newImage1 = image.getImage();                       // Transforms image.
         Image newImage2 = newImage1.getScaledInstance(871, 555,  java.awt.Image.SCALE_SMOOTH); // Scales the image. 
         ImageIcon icon = new ImageIcon(newImage2);                // Adds the scaled image to icon 
-        
+        JLabel label = new JLabel(icon);
+		
         label.setIcon(icon);                                      // Adds icon to label.
         label.setHorizontalAlignment(JLabel.CENTER);              // Moves image to the center horizontally.
         label.setVerticalAlignment(JLabel.CENTER);                // Moves image to the center vertically.
-        frame.add(label);                                         // Adds label to frame to display image. 
+        frame.add(label);   
+		
+		frame.setVisible(true);                              // Makes the window visible.
+		
+		try{Thread.sleep(t);}catch(Exception e){}
+		
+		frame.setVisible(false);
+		
     }
-
 }
