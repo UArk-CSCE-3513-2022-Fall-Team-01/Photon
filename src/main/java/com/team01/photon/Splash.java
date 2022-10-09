5128e327f94ce60a344350a2084cc61c6e3d6429
bakinser <bakinser@gmail.com>
2022-10-09 15:47:12 -0500

    Fix splash class merge conflict

--- src/main/java/com/team01/photon/Splash.java
+++ src/main/java/com/team01/photon/Splash.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 class Splash {
 
-	private int t; //The time before the splash screen hides
+	private final int t; //The time before the splash screen hides
 	
     public Splash(int time){
 		t = time;
@@ -15,25 +15,23 @@ class Splash {
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setSize(871, 555);                 // Sets the size of the window.
         frame.setLocationRelativeTo(null);                   // Sets the location to the center of the screen. 
-        frame.setResizable(false);                   // Prevents the manual resize of the window. 
-    }
-    public void addImage(){        
-        ImageIcon image = new ImageIcon(this.getClass().getResource("images/photon.jpg"));    // Creates ImageIcon component. 
+        frame.setResizable(false);                   // Prevents the manual resize of the window.
+
+        ImageIcon image = new ImageIcon(this.getClass().getResource("/images/photon.jpg"));    // Creates ImageIcon component.
         Image newImage1 = image.getImage();                       // Transforms image.
-        Image newImage2 = newImage1.getScaledInstance(871, 555,  java.awt.Image.SCALE_SMOOTH); // Scales the image. 
-        ImageIcon icon = new ImageIcon(newImage2);                // Adds the scaled image to icon 
+        Image newImage2 = newImage1.getScaledInstance(871, 555,  java.awt.Image.SCALE_SMOOTH); // Scales the image.
+        ImageIcon icon = new ImageIcon(newImage2);                // Adds the scaled image to icon
         JLabel label = new JLabel(icon);
-		
+
         label.setIcon(icon);                                      // Adds icon to label.
         label.setHorizontalAlignment(JLabel.CENTER);              // Moves image to the center horizontally.
         label.setVerticalAlignment(JLabel.CENTER);                // Moves image to the center vertically.
-        frame.add(label);   
-		
-		frame.setVisible(true);                              // Makes the window visible.
-		
-		try{Thread.sleep(t);}catch(Exception e){}
-		
-		frame.setVisible(false);
-		
+        frame.add(label);
+
+        frame.setVisible(true);                              // Makes the window visible.
+
+        try{Thread.sleep(t);}catch(Exception e){e.printStackTrace();}
+
+        frame.setVisible(false);
     }
 }
