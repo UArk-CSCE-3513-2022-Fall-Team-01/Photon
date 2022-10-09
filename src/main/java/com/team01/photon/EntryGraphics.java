484da21f4a0445af42aec756b2248e6521391599
bakinser <bakinser@gmail.com>
2022-10-09 16:58:37 -0500

    Add selection box and Tab key input

--- src/main/java/com/team01/photon/EntryGraphics.java
+++ src/main/java/com/team01/photon/EntryGraphics.java
@@ -1,16 +1,49 @@
 package com.team01.photon;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class EntryGraphics extends JPanel
+public class EntryGraphics extends JPanel implements KeyListener
 {
     private final int frameW;
     private final int frameH;
+    private int playerSelected;
+    private boolean teamSelected; //False for red, true for green
+    private boolean editingCodename; //False means entering ID, true means entering/editing codename
+
+    //Spacing variables
+    private final int pixelsBetweenBoxes;
+    private final int boxHeight;
+    private final int idWidth;
+    private final int playerWidth;
+    private final int redTeamX; //X value to start drawing the selection box for red team
+    private final int blueTeamX; //X value to start drawing the selection box for blue team
+    private final int startingY; //Y value to start drawing the selection box
+
+    //Keys
+    boolean tabKeyDown;
+
 
     public EntryGraphics(int width, int height)
     {
         frameW = width;
         frameH = height;
+        playerSelected = 0;
+        teamSelected = false;
+        editingCodename = false;
+
+        //Spacing variables
+        pixelsBetweenBoxes = 5;
+        boxHeight = 40;
+        idWidth = 90;
+        playerWidth = 250;
+        redTeamX = 45;
+        blueTeamX = 445;
+        startingY = 120;
+
+        //Keys
+        tabKeyDown = false;
+
     }
 
     @Override
@@ -24,5 +57,30 @@ public class EntryGraphics extends JPanel
         ImageIcon entryImage = new ImageIcon(this.getClass().getResource("/images/entryscreen.png"));
         g.drawImage(entryImage.getImage(),0,0,null);
 
+        //Selection box outline
+        g.setColor(System.currentTimeMillis() % 700 > 350 ? Color.BLUE : Color.WHITE);
+        for(int outlineLayer = 0; outlineLayer < 5; outlineLayer++)
+        {
+            int x = (teamSelected ? blueTeamX : redTeamX) + (editingCodename ? idWidth : 0) + outlineLayer;
+            int y = startingY + playerSelected*(pixelsBetweenBoxes+boxHeight) + outlineLayer;
+            int w = (editingCodename ? playerWidth : idWidth) + (9-outlineLayer*2);
+            int h = boxHeight + (9-outlineLayer*2);
+            g.drawRect(x,y,w,h);
+        }
     }
+
+    public void keyPressed(KeyEvent ke)
+    {
+        System.out.println(ke.getKeyCode());
+        switch(ke.getKeyCode())
+        {
+            case KeyEvent.VK_TAB:
+                playerSelected++;
+                break;
+        }
+    }
+    public void keyReleased(KeyEvent ke)
+    {
+    }
+    public void keyTyped(KeyEvent ke){}
 }
--- src/main/java/com/team01/photon/Game.java
+++ src/main/java/com/team01/photon/Game.java
@@ -1,28 +1,52 @@
 package com.team01.photon;
 import javax.swing.*;
-public class Game{
-    public static void main(String[] args){
-        Splash splash = new Splash(3000); //Create a splash screen that lasts for 3 seconds
-		splash.show(); //Show the splash screen
-
+public class Game extends JFrame
+{
+    public EntryGraphics entryScreen;
+    public Game()
+    {
         int frameW = 800;
         int frameH = 1000;
 
-        JFrame frame = new JFrame("Photon");
-        EntryGraphics entryScreen = new EntryGraphics(frameW,frameH);
-        frame.add(entryScreen);
+        entryScreen = new EntryGraphics(frameW,frameH);
 
-        frame.pack();
-        frame.setSize(frameW, frameH);                     // Size frame.
-        frame.setResizable(false);                         // Prevents the manual resize of the window.
-        frame.setLocationRelativeTo(null);                         // Places frame in the center of the screen.
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);         // Finishes the program when we press the X button.
+        //JFrame settings
+        this.setTitle("Photon");
+        this.add(entryScreen); //Display the entry screen graphics first
+        this.pack();
+        this.setSize(frameW,frameH);
+        this.setResizable(false); //Makes window unresizable
+        this.setLocationRelativeTo(null); // Places frame in the center of the screen.
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE); // Finishes the program when we press the X button.
+        this.setFocusTraversalKeysEnabled(false); //Required to capture Tab key inputs
+        this.setVisible(true); //Make it appear
+
+        //Add a key listener for keyboard input
+        this.addKeyListener(entryScreen);
+
+    }
+
+    public void run()
+    {
+        while(true)
+        {
+            entryScreen.repaint();
 
 
-        frame.setVisible(true);
+            try
+            {
+                Thread.sleep(40);
+            } catch(Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+    public static void main(String[] args){
+        Splash splashScreen = new Splash(3000); //Create a splash screen that lasts for 3 seconds
+		splashScreen.show(); //Show the splash screen
 
-
-        //Entry screen manipulation
-        boolean team = false; //False is editing team red side, true is editing team blue side
+        Game game = new Game();
+        game.run();
     }
 }
