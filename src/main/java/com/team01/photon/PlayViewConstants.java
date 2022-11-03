19ff82f5d7ade006ff700b65c9a7eca087707804
Adam Sneed <awsneed@uark.edu>
2022-11-03 13:23:14 -0500

    Introduce PlayViewConstants class

---
+++ src/main/java/com/team01/photon/PlayViewConstants.java
@@ -0,0 +1,11 @@
+package com.team01.photon;
+
+import java.awt.Color;
+
+public final class PlayViewConstants {
+    private PlayViewConstants() {
+        // Hide constructor, class only contains static constants
+    }
+
+    public static final Color BG_COLOR = Color.BLACK;
+}
