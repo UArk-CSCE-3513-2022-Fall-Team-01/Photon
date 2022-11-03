162880a389062ef9cc2534e196a115609a8d2dad
Adam Sneed <awsneed@uark.edu>
2022-11-03 13:38:43 -0500

    Update PlayViewConstants

--- src/main/java/com/team01/photon/PlayViewConstants.java
+++ src/main/java/com/team01/photon/PlayViewConstants.java
@@ -1,11 +1,20 @@
 package com.team01.photon;
 
 import java.awt.Color;
+import java.awt.Font;
 
 public final class PlayViewConstants {
     private PlayViewConstants() {
         // Hide constructor, class only contains static constants
     }
 
+    private static final float EM = (float)1.2;
+    private static final float DE_EM = (float)0.8;
+
     public static final Color BG_COLOR = Color.BLACK;
+    public static final Color FG_COLOR = Color.WHITE;
+    public static final Font BASE_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 28);
+    public static final Font BIG_FONT = BASE_FONT.deriveFont(BASE_FONT.getSize() * EM);
+    public static final Font SMALL_FONT = BASE_FONT.deriveFont(BASE_FONT.getSize() * DE_EM);
+
 }
